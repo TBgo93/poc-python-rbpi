ee2d27a68e42bfa622a99b6c4669aaffd7466346
TBgo93 <tbaigorria93@gmail.com>
2023-10-21 17:29:36 -0300

    Add def main & try except

--- text.py
+++ text.py
@@ -11,37 +11,41 @@ IP = "IP: "
 CPU = "% CPU: "
 RAM = "% RAM: "
 
+def __main__ ():
+  disp = ST7789.ST7789(
+    height=240,
+    width=240,
+    rotation=90,
+    port=0,
+    cs=1,
+    dc=9,
+    backlight=13,
+    spi_speed_hz=60 * 1000 * 1000,
+    offset_left=0,
+    offset_top=0
+  )
 
-# Create ST7789 LCD display class.
+  # Initialize display.
+  disp.begin()
 
-disp = ST7789.ST7789(
-  height=240,
-  width=240,
-  rotation=90,
-  port=0,
-  cs=1,
-  dc=9,
-  backlight=13,
-  spi_speed_hz=60 * 1000 * 1000,
-  offset_left=0,
-  offset_top=0
-)
+  WIDTH = disp.width
+  HEIGHT = disp.height
 
-# Initialize display.
-disp.begin()
+  img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
 
-WIDTH = disp.width
-HEIGHT = disp.height
+  draw = ImageDraw.Draw(img)
 
-img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+  font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 20)
 
-draw = ImageDraw.Draw(img)
+  while True:
+    draw.rectangle((0, 0, disp.width, disp.height), (0, 0, 0))
+    draw.text((5, 1), IP, font=font, fill=(255, 255, 255))
+    draw.text((5, 31), CPU, font=font, fill=(255, 255, 255))
+    draw.text((5, 61), RAM, font=font, fill=(255, 255, 255))
+    disp.display(img)
 
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 20)
-
-while True:
-  draw.rectangle((0, 0, disp.width, disp.height), (0, 0, 0))
-  draw.text((5, 5), IP, font=font, fill=(255, 255, 255))
-  draw.text((5, 35), CPU, font=font, fill=(255, 255, 255))
-  draw.text((5, 65), RAM, font=font, fill=(255, 255, 255))
-  disp.display(img)
+try:
+  __main__
+except KeyboardInterrupt:
+  print("Exiting the program...")
+  exit(1)
