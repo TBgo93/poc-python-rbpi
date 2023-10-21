d01b8353124bc329d91cfa07c3b62ee457c96465
TBgo93 <tbaigorria93@gmail.com>
2023-10-21 19:11:02 -0300

    Refactor

--- text.py
+++ text.py
@@ -9,12 +9,28 @@ from PIL import ImageFont
 import ST7789
 
 
-def display():
+def display_text(WIDTH, HEIGHT):
   IP = "IP: "
   CPU = "% CPU: "
   RAM = "% RAM: "
   TIME = strftime("%d %b %Y %H:%M:%S", localtime())
 
+  img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+
+  draw = ImageDraw.Draw(img)
+
+  font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 20)
+
+  font_datetime = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 14)
+
+  draw.rectangle((0, 0, WIDTH, HEIGHT), (0, 0, 0))
+  draw.text((5, 1), IP, font=font, fill=(255, 255, 255))
+  draw.text((5, 31), CPU, font=font, fill=(255, 255, 255))
+  draw.text((5, 61), RAM, font=font, fill=(255, 255, 255))
+  draw.text((5, 220), TIME, font=font_datetime, fill=(255, 255, 255))
+  return img
+
+def main():
   disp = ST7789.ST7789(
     height=240,
     width=240,
@@ -34,24 +50,9 @@ def display():
   WIDTH = disp.width
   HEIGHT = disp.height
 
-  img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
-
-  draw = ImageDraw.Draw(img)
-
-  font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 20)
-
-  font_datetime = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 14)
-
-  draw.rectangle((0, 0, disp.width, disp.height), (0, 0, 0))
-  draw.text((5, 1), IP, font=font, fill=(255, 255, 255))
-  draw.text((5, 31), CPU, font=font, fill=(255, 255, 255))
-  draw.text((5, 61), RAM, font=font, fill=(255, 255, 255))
-  draw.text((5, 220), TIME, font=font_datetime, fill=(255, 255, 255))
-  disp.display(img)
-
-def main():
   while True:
-    display()
+    img = display_text(WIDTH, HEIGHT)
+    disp.display(img)
     sleep(5)
 
 try:
