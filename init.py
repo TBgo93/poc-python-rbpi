33435ca98edc8aa0cee83de719328f2171eead27
TBgo93 <tbaigorria93@gmail.com>
2023-10-29 16:29:40 -0300

    Log

--- init.py
+++ init.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
   # Handle buttons
   for pin in BUTTONS:
     GPIO.add_event_detect(pin, GPIO.FALLING, handle_button, bouncetime=100)
+    print(GPIO.event_detected(pin))
     if pin == 5 | pin == 6:
       isExecutable = GPIO.event_detected(pin)
 
