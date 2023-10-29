3ffc180c5f5b98316bc61b06356996be8716c7a9
TBgo93 <tbaigorria93@gmail.com>
2023-10-29 16:31:28 -0300

    Fix sigint

--- init.py
+++ init.py
@@ -3,7 +3,7 @@ import signal
 import RPi.GPIO as GPIO
 
 from display import display_empty, main
-from buttons import handle_button, BUTTONS, LABELS
+from buttons import handle_button, BUTTONS
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -25,7 +25,10 @@ if __name__ == "__main__":
     print(GPIO.event_detected(pin))
     if pin == 5 | pin == 6:
       isExecutable = GPIO.event_detected(pin)
+  
+  def init():
+    main(isExecutable)
 
-  t = threading.Thread(target=main(isExecutable))
+  t = threading.Thread(target=init)
   t.start()
   t.join()
